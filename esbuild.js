a887ee5990cdda0f0d25632dd82fd9077a428453
Eric McCarthy <eric@limulus.net>
2024-10-25 17:19:50 +0000

    feat: build page specific js bundles

--- esbuild.js
+++ esbuild.js
@@ -3,16 +3,18 @@ import { wasmLoader } from 'esbuild-plugin-wasm'
 import { globby } from 'globby'
 import { basename, join, sep, parse } from 'node:path'
 
-const outdir = 'dist/www/assets/js'
-const entryPoints = await globby(['src/**/index.ts', 'src/**/worker.ts'])
+const entryPoints = await globby(['src/**/index.ts', 'src/**/worker.ts', 'www/**/index.ts'])
 
 await Promise.all(
   entryPoints.map(async (entryPoint) => {
-    const dir = parse(entryPoint).dir.split(sep).pop()
-    const outfile = join(outdir, dir, `${basename(entryPoint, '.ts')}.js`)
+    const lib = parse(entryPoint).dir.split(sep).pop()
+    const outfile = entryPoint.startsWith('www')
+      ? join('dist', parse(entryPoint).dir, `${basename(entryPoint, '.ts')}.js`)
+      : join('dist/www/assets/js', lib, `${basename(entryPoint, '.ts')}.js`)
 
     await build({
       entryPoints: [entryPoint],
+      external: ['/assets/js/*'],
       outfile,
       bundle: true,
       format: 'esm',
