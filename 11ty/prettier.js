aa251e223f385928966e7fa82d2cf363a101bdca
Eric McCarthy <eric@limulus.net>
2024-11-17 18:57:25 +0000

    feat: prettier options

--- 11ty/prettier.js
+++ 11ty/prettier.js
@@ -5,7 +5,7 @@ export default function prettierPlugin(eleventyConfig) {
   eleventyConfig.addTransform('prettier', async function (content) {
     try {
       if (this.page.outputPath.endsWith('.html')) {
-        content = await format(content, { parser: 'html' })
+        content = await format(content, { parser: 'html', semi: false, singleQuote: true })
       }
     } catch (error) {
       console.warn(`Error formatting ${this.page.inputPath}: ${error.message}`)
